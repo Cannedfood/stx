aa8fcd5b8739649ea1129c4a8dc7b0795e05a92c
Benno Straub <benno.straub@outlook.de>
2017-05-26 16:17:38 +0200

    Implemented memory.hpp further

--- include/stx/memory.hpp
+++ include/stx/memory.hpp
@@ -6,6 +6,10 @@
 
 #include "type.hpp"
 
+#ifdef STX_DEBUG
+#	include "assert.hpp"
+#endif
+
 namespace stx {
 
 class refcounted_interface;
@@ -23,13 +27,15 @@ template<typename T, typename Tdel = stx::default_delete<T>>
 class owned {
 private:
 	using Tself = owned<T, Tdel>;
+	using Tptr  = pointer_to<T>;
+	using Tref  = remove_pointer<Tptr>;
 
-	mutable T*   m_pointer;
+	mutable Tptr m_pointer;
 	mutable Tdel m_deleter;
 
 public:
 	constexpr inline
-	owned(T* ptr = nullptr, const Tdel& del = Tdel()) :
+	owned(Tptr ptr = nullptr, const Tdel& del = Tdel()) :
 		m_pointer(ptr),
 		m_deleter(del)
 	{}
@@ -58,7 +64,7 @@ public:
 	Tself& operator=(Tself const&) = delete;
 
 	constexpr inline
-	void reset(T* ptr = nullptr, const Tdel& del = Tdel()) {
+	void reset(Tptr ptr = nullptr, const Tdel& del = Tdel()) {
 		if(m_pointer) {
 			m_deleter(m_pointer);
 		}
@@ -67,16 +73,32 @@ public:
 	}
 
 	constexpr inline
-	operator bool() const {
+	Tptr release() noexcept {
+		auto tmp = std::move(m_pointer);
+		m_pointer = nullptr;
+		return tmp;
+	}
+
+	constexpr inline
+	operator bool() const noexcept {
 		return m_pointer != nullptr;
 	}
 
 	constexpr inline
-	T* get() const { return m_pointer; }
+	T* get() const noexcept { return m_pointer; }
 
-	T* operator->() const { return m_pointer; }
-	T& operator*()  const { return *m_pointer; }
+	constexpr inline
+	T* operator->() const noexcept { return m_pointer; }
+
+	constexpr inline
+	T& operator*()  const noexcept { return *m_pointer; }
 };
+
+template<typename T, typename Tdel = stx::default_delete<T>> constexpr inline
+owned<T, Tdel> own(T* t, Tdel const& del = Tdel()) noexcept { return owned<T, Tdel>(t, del); }
+
+template<typename T, typename... ARGS>
+owned<T> new_owned(ARGS&&... args) noexcept { return owned<T>(new T(std::forward<ARGS>(args)...)); }
 
 
 // == shared<T> ==============================================================
@@ -89,7 +111,6 @@ private:
 	unsigned m_weak_refs;
 
 protected:
-
 	constexpr inline
 	shared_block() :
 		m_shared_refs(0),
@@ -126,14 +147,14 @@ protected:
 	virtual void destroy_block() = 0;
 
 	struct shared_ownership {
-		constexpr inline
+		inline
 		void operator()(shared_block* b) const noexcept {
 			b->decrement_shared();
 		}
 	};
 
 	struct weak_ownership {
-		constexpr inline
+		inline
 		void operator()(shared_block* b) const noexcept {
 			b->decrement_weak();
 		}
@@ -162,27 +183,127 @@ public:
 };
 
 template<typename T, typename Tdel = stx::default_delete<T>>
-class simple_shared_block : public shared_block, owned<T, Tdel> {
-	T* m_pointer;
+class simple_shared_block : public shared_block {
+	using Tptr = pointer_to<T>;
+
+	Tptr m_pointer;
+	Tdel m_deleter;
 
 public:
-	T* pointer() noexcept { return m_pointer; }
+	constexpr
+	simple_shared_block(Tptr ptr, const Tdel& del = Tdel()) :
+		m_pointer(ptr),
+		m_deleter(m_deleter)
+	{}
 
+	inline Tptr pointer() noexcept { return m_pointer; }
+
+	void destroy_pointer() override {
+		m_deleter(m_pointer);
+	}
+
+	void destroy_block()   override {
+		delete this;
+	}
+};
+
+template<typename T>
+class aligned_shared_block : public shared_block {
+	char m_data[sizeof(T)];
+
+public:
+	template<typename... ARGS> constexpr
+	aligned_shared_block(ARGS&&... args) {
+#ifdef STX_DEBUG
+		xassertmsg(
+			new (m_data) T(std::forward<ARGS>(args)...) == pointer(),
+			"STX code error! Please open an issue with reproduction information on github!"
+		);
+#else
+		new (m_data) T(std::forward<ARGS>(args)...);
+#endif
+	}
+
+	~aligned_shared_block() {
+		reinterpret_cast<T*>(m_data)->~T();
+	}
+
+	inline T* pointer() noexcept { return reinterpret_cast<T*>(m_data); }
+
+	void destroy_pointer() override {
+		pointer()->~T();
+	}
+
+	void destroy_block()   override {
+		delete this;
+	}
 };
 
 } // namespace detail
 
 template<typename T>
 class shared {
+	using Tself = shared<T>;
+	using Tptr  = pointer_to<T>;
+	using Tref  = remove_pointer<Tptr>;
+
 	detail::shared_block::shared_ref m_shared_block;
-	T*                               m_pointer;
+	mutable Tptr                     m_pointer;
 
 public:
-	shared()  {}
+	constexpr
+	shared() :
+		m_pointer(nullptr)
+	{}
+
 	~shared() {}
 
-	T* get() const { return m_pointer; }
+	shared(Tself const& other) :
+		m_shared_block(other.m_shared_block ? m_shared_block->new_shared_ref() : nullptr),
+		m_pointer(other.m_pointer)
+	{}
+
+	shared(Tself&& other) :
+		m_shared_block(std::move(other.m_shared_block)),
+		m_pointer(other.m_pointer)
+	{
+		other.m_pointer = nullptr;
+	}
+
+	Tself& operator=(Tself const& other) noexcept {
+		m_shared_block  = other.m_shared_block ? other.m_shared_block->new_shared_ref() : nullptr;
+		m_pointer       = other.m_pointer;
+		other.m_pointer = nullptr;
+		return *this;
+	}
+
+	Tself& operator=(Tself&& other) noexcept {
+		m_shared_block  = std::move(other.m_shared_block);
+		m_pointer       = other.m_pointer;
+		other.m_pointer = nullptr;
+		return *this;
+	}
+
+	inline Tptr get()        const noexcept { return m_pointer; }
+	inline Tptr operator->() const noexcept { return m_pointer; }
+	inline Tref operator*()  const noexcept { return *m_pointer; }
+
+	template<typename Idx, typename = typename std::enable_if<is_array<T>>>
+	Tref operator[](Idx i) const noexcept {
+		return m_pointer[i];
+	}
 };
+
+template<typename T, typename Tdel = stx::default_delete<T>>
+owned<T, Tdel> share(T* t, Tdel const& del = Tdel()) noexcept {
+	return share<T>(new detail::simple_shared_block<T, Tdel>(t, del), t);
+}
+
+template<typename T, typename... ARGS>
+owned<T> new_shared(ARGS&&... args) noexcept {
+	auto* block = new detail::aligned_shared_block<T>(std::forward<ARGS>(args)...);
+	return share<T>(block, block->pointer());
+}
 
 
 // == weak<T> ==============================================================
@@ -190,19 +311,47 @@ public:
 template<typename T>
 class weak {
 	using Tself = weak<T>;
+	using Tshared = shared<T>;
 
 	mutable detail::shared_block::weak_ref m_shared_block;
 	mutable T* m_pointer;
 
 public:
 	weak(detail::shared_block* block, T* ptr) :
-		m_shared_block(block ? block->new_weak_ref() : nullptr),
-		m_pointer(ptr)
-	{}
+		m_shared_block(),
+		m_pointer(nullptr)
+	{
+		if(block && block->shared_refs()) {
+			m_shared_block = block->new_weak_ref();
+			m_pointer      = ptr;
+		}
+		else {
+			m_shared_block = nullptr;
+			m_pointer      = nullptr;
+		}
+	}
 
-	weak(const Tself& other) {
+	void reset(detail::shared_block* block, T* ptr) {
+		if(block && block->shared_refs()) {
+			m_shared_block = block->new_weak_ref();
+			m_pointer      = ptr;
+		}
+		else {
+			m_shared_block = nullptr;
+			m_pointer      = nullptr;
+		}
+	}
+
+	void reset() {
+		m_shared_block = nullptr;
+		m_pointer      = nullptr;
+	}
+
+	weak(Tself const& other) :
+		weak(other.m_shared_block.get(), other.pointer)
+	{
 		if(other.m_shared_block && other.m_shared_block->shared_refs())
-			m_shared_block = other.m_shared_block->new_shared_ref();
+			m_shared_block = other.m_shared_block->new_weak_ref();
 		m_pointer = other.m_pointer;
 	}
 
@@ -211,11 +360,18 @@ public:
 		m_pointer(other)
 	{}
 
-	void operator=(const Tself& other) {
+	weak(Tshared const& s) :
+		weak()
+	{}
 
+	void operator=(Tself const& other) {
+		reset(other.m_shared_block.get(), other.m_pointer);
 	}
 
-	~weak() {}
+	void operator=(Tself&& other) {
+		reset(other.m_shared_block.release(), other.m_pointer);
+		other.m_pointer = nullptr;
+	}
 
 	operator bool() const {
 		if(m_shared_block && m_shared_block->shared_refs())
@@ -234,6 +390,7 @@ public:
 			return nullptr;
 	}
 
+	inline
 	T* get() const noexcept {
 		if(*this)
 			return m_pointer;
@@ -241,8 +398,8 @@ public:
 			return nullptr;
 	}
 
-	T* operator->() const { return get(); }
-	T& operator*()  const { return *get(); }
+	inline T* operator->() const noexcept { return get(); }
+	inline T& operator*()  const noexcept { return *get(); }
 };
 
 } // namespace stx
--- include/stx/type.hpp
+++ include/stx/type.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <type_traits>
+
 namespace stx {
 
 namespace detail {
@@ -20,11 +22,17 @@ template<typename T>
 using pointer_to = typename detail::pointer_to_struct<T>::type;
 
 template<typename T>
+using remove_pointer = typename std::remove_pointer<T>::type;
+
+template<typename T> constexpr static
+bool is_array = std::is_array<T>::value;
+
+template<typename T>
 struct default_delete {
 	using pointer = pointer_to<T>;
 
 	inline
-	void operator()(pointer p) const noexcept {
+	void operator()(pointer p) const {
 		delete p;
 	}
 };
