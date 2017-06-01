ff9913f2208d227b55aa4dad5a5b261e1bf4e78d
Benno Straub <benno.straub@outlook.de>
2017-06-01 20:55:40 +0200

    Removed unnecessary inline keywords in access.hpp

--- include/stx/access.hpp
+++ include/stx/access.hpp
@@ -1,17 +1,17 @@
 #pragma once
 
 #define xgetter(ACCESSOR, MEMBER) \
-	inline auto const& ACCESSOR() const noexcept { return MEMBER; }
+	auto const& ACCESSOR() const noexcept { return MEMBER; }
 
-#define xsetter(ACCESSOR, MEMBER)                 \
-	inline auto& ACCESSOR(decltype(MEMBER)& m) {  \
-		MEMBER = m;                               \
-		return *this;                             \
-	}                                             \
-                                                  \
-	inline auto& ACCESSOR(decltype(MEMBER)&& m) { \
-		MEMBER = std::move(m);                    \
-		return *this;                             \
+#define xsetter(ACCESSOR, MEMBER)                \
+	auto& ACCESSOR(decltype(MEMBER) const & m) { \
+		MEMBER = m;                              \
+		return *this;                            \
+	}                                            \
+                                                 \
+	auto& ACCESSOR(decltype(MEMBER)&& m) {       \
+		MEMBER = std::move(m);                   \
+		return *this;                            \
 	}
 
 #define xaccess(ACCESSOR, MEMBER) \
