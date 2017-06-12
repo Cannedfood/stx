a7015573983b8ae7a14326a442560acf4671b1e3
Benno Straub <benno.straub@outlook.de>
2017-06-12 21:35:49 +0200

    Added stx/wip -> xwip to update.sh

--- update.sh
+++ update.sh
@@ -2,13 +2,23 @@
 
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-rm "${scriptdir}/include/"* 2> /dev/null
+rm -r "${scriptdir}/include/x"* 2> /dev/null
 
-for header in "${scriptdir}/include/stx/"*.hpp
+for header in "${scriptdir}/include/stx/"*
 do
 	if [ ! -d "${header}" ]
 	then
 		name=`basename ${header%.*}`
 		printf '#include "%s"' "stx/${name}.hpp" > "${scriptdir}/include/x${name}"
+	fi
+done
+
+mkdir "${scriptdir}/include/xwip/"
+for header in "${scriptdir}/include/stx/wip/"*.hpp
+do
+	if [ ! -d "${header}" ]
+	then
+		name=`basename ${header%.*}`
+		printf '#include "%s"' "../stx/wip/${name}.hpp" > "${scriptdir}/include/xwip/${name}"
 	fi
 done
