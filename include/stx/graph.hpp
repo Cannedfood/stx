e2b0e9cdeea75bf5e29d6281f9f5363ef0cbb60c
Benno Straub <benno.straub@outlook.de>
2017-06-12 23:00:31 +0200

    Updated xhandles -> xgraph

--- include/stx/graph.hpp
+++ include/stx/graph.hpp
@@ -82,4 +82,74 @@ public:
 	const T* last() const noexcept { return m_last; }
 };
 
+
+#ifdef STX_WIP
+
+template<typename T, typename TParent>
+class child_element;
+
+template<typename T, typename TChild>
+class parent_element {
+	using Tself  = parent_element<T, TChild>;
+
+	TChild* m_children;
+
+	friend T;
+	friend class child_element<TChild, parent_element<T, TChild>>;
+protected:
+	~parent_element();
+
+public:
+	TChild*       children()       { return m_children; }
+	TChild const* children() const { return m_children; }
+
+	parent_element() :
+		m_children()
+	{}
+
+	parent_element(Tself const&) = delete;
+
+	parent_element(Tself&& e) :
+		m_children(e.m_children)
+	{
+		e.m_children = nullptr;
+	}
+
+	Tself& operator=(Tself const&) = delete;
+	Tself& operator=(Tself&& other) {
+		std::swap(m_children, other.m_children);
+	}
+};
+
+template<typename T, typename TParent>
+class child_element : public list_element<T> {
+	using parent_type = TParent;
+
+	static_assert(std::is_base_of<parent_element<TParent, T>, TParent>::value, "TParent must inherit parent_element<T>");
+
+	parent_type* m_parent;
+
+	friend T;
+	friend TParent;
+	friend class parent_element<T, child_element<T, TParent>>;
+protected:
+	child_element() {}
+	~child_element() {
+		// remove();
+	}
+
+public:
+	parent_type parent() { return m_parent; }
+};
+
+template<typename T>
+class tree_node :
+	public parent_element<T, T>,
+	public child_element <T, T>
+{
+	// should need nothing but constructor & destructor
+};
+
+#endif // STX_WIP
+
 } // namespace stx
--- test/xgraph.cpp
+++ test/xgraph.cpp
@@ -1,4 +1,4 @@
-#include <xhandles>
+#include <xgraph>
 
 #include "test.hpp"
 
