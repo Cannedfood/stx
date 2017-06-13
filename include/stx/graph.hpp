afff406ed25055e8744d7c160e663f06f0699e6c
Benno Straub <benno.straub@outlook.de>
2017-06-13 11:23:48 +0200

    Got pointer_pair in graph.hpp working & wrote tests for it

--- include/stx/graph.hpp
+++ include/stx/graph.hpp
@@ -2,6 +2,10 @@
 
 #include <type_traits>
 
+#ifdef STX_DEBUG
+#	include "assert.hpp"
+#endif
+
 namespace stx {
 
 template<typename T>
@@ -82,6 +86,50 @@ public:
 	const T* last() const noexcept { return m_last; }
 };
 
+template<typename T>
+class pointer_pair {
+	using Tself  = pointer_pair<T>;
+	using Tother = T;
+
+	T* m_other;
+
+public:
+	pointer_pair(Tself const&) = delete;
+
+	pointer_pair() : m_other(nullptr) {}
+	pointer_pair(Tself&& other) :
+		pointer_pair()
+	{
+		*this = std::move(other);
+	}
+
+	~pointer_pair() noexcept {
+		reset();
+	}
+
+	Tself& operator=(Tself const&) = delete;
+	Tself& operator=(Tself&& other) noexcept {
+		reset(other.m_other);
+		return *this;
+	}
+
+	void reset(T* other = nullptr) noexcept {
+		if(m_other) {
+#ifdef STX_DEBUG
+			xassert(m_other->m_other == this);
+#endif
+			m_other->m_other = nullptr;
+		}
+
+		m_other = other;
+		if(m_other) {
+			m_other->reset();
+			m_other->m_other = (decltype(m_other->m_other)) this;
+		}
+	}
+
+	T* get() noexcept { return m_other; }
+};
 
 #ifdef STX_WIP
 
--- test/xgraph.cpp
+++ test/xgraph.cpp
@@ -1,6 +1,10 @@
+#define STX_WIP
+
 #include <xgraph>
 
 #include "test.hpp"
+
+using namespace stx;
 
 static
 void test_list_element() {
@@ -17,8 +21,38 @@ void test_tree() {
 	// TODO
 }
 
+static
+void test_pointer_pair() {
+	struct twin : public pointer_pair<twin> {};
+
+	twin a;
+	twin c;
+
+	test(!a.get());
+
+	{
+		twin b;
+		a.reset(&b);
+		test(b.get() == &a);
+		test(a.get() == &b);
+		test(c.get() == nullptr);
+	}
+
+	test(a.get() == nullptr);
+
+	{
+		twin b;
+		a.reset(&b);
+		c = std::move(a);
+		test(a.get() == nullptr);
+		test(c.get() == &b);
+		test(b.get() == &c);
+	}
+}
+
 void test_xhandles() {
 	test_list_element();
 	test_parent_child();
 	test_tree();
+	test_pointer_pair();
 }
