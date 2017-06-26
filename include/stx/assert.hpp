a13494900e096ff53893c9dcd1103390e9866277
Benno Straub <benno.straub@outlook.de>
2017-06-26 13:01:15 +0200

    Made xfatal actually fatal

--- include/stx/assert.hpp
+++ include/stx/assert.hpp
@@ -2,14 +2,24 @@
 
 #include "platform.hpp"
 
-#include <stdexcept>
+#include <exception>
 #include <string>
 
 
-#define xfatal(MSG)                                                                                \
-	do {                                                                                           \
-		throw std::runtime_error(std::string(__FILE__) + ":" + std::to_string(__LINE__) + " in " + \
-		                         STX_FUNCTION + " : " + MSG);                                      \
+#define xfatal(MSG)                                                     \
+	do {                                                                \
+		fputs((std::string(__FILE__) + ":" + std::to_string(__LINE__) + \
+		       " in " + STX_FUNCTION + " : " + MSG)                     \
+		          .c_str(),                                             \
+		      stderr);                                                  \
+		std::terminate();                                               \
+	} while(false)
+
+#define xfail(MSG)                                                   \
+	do {                                                             \
+		throw std::runtime_error(std::string(__FILE__) + ":" +       \
+		                         std::to_string(__LINE__) + " in " + \
+		                         STX_FUNCTION + " : " + MSG);        \
 	} while(false)
 
 #ifdef STX_DEBUG_TOOLS
@@ -32,9 +42,13 @@
 #else // defined(STX_DEBUG_TOOLS)
 
 #define xassert(TEST) \
-	do { if(TEST) {} } while(false) // The if is necessary to supress warnings
+	do {              \
+		if(TEST) {}   \
+	} while(false) // The if is necessary to supress warnings
 #define xassertmsg(TEST, MSG) \
-	do { if(TEST) {} } while(false)
+	do {                      \
+		if(TEST) {}           \
+	} while(false)
 
 #endif // defined(STX_DEBUG_TOOLS)
 
@@ -44,52 +58,59 @@
 
 namespace stx {
 
-/// A class used for debugging which checks whether a returned value was handled
+/// A class used for debugging which checks whether a returned value was
+/// handled
 template <typename T>
 class important {
 	T            m_value;
 	mutable bool m_handled;
 
 public:
-	template<typename Tx> constexpr
-	important(Tx&& val) : m_value(val), m_handled(false) {}
+	template <typename Tx>
+	constexpr important(Tx&& val) : m_value(val), m_handled(false) {}
 
 	~important() noexcept {
 		if(!m_handled) {
-			stx::warn("Unhandled important value! (important function result not handled)");
+			stx::warn(
+			    "Unhandled important value! (important function result not "
+			    "handled)");
 		}
 	}
 
-	constexpr inline
-	operator T() const noexcept {
+	constexpr inline operator T() const noexcept {
 		m_handled = true;
 		return std::move(m_value);
 	}
 };
 
-/// A class used for debugging which checks whether a returned value was handled
+/// A class used for debugging which checks whether a returned value was
+/// handled
 template <typename T>
 class asserted {
 	T            m_value;
 	mutable bool m_handled;
 
 public:
-	template<typename Tx> constexpr
-	asserted(Tx&& val) : m_value(val), m_handled(false) {}
+	template <typename Tx>
+	constexpr asserted(Tx&& val) : m_value(val), m_handled(false) {}
 
 	~asserted() {
 		if(!m_handled) {
 			if(!m_value) {
-				xfatal("Asserted return value was not handled and evaluates to false");
-			}
-			else {
-				stx::warn("Unhandled important value! (important function result not handled)");
+				xfatal(
+				    "Asserted return value was not handled and evaluates "
+				    "to "
+				    "false");
+			} else {
+				stx::warn(
+				    "Unhandled important value! (important function result "
+				    "not "
+				    "handled)");
 			}
 		}
 	}
 
-	constexpr inline
-	operator T() const noexcept {
+	constexpr inline operator T() const noexcept {
 		m_handled = true;
 		return std::move(m_value);
 	}
@@ -97,20 +118,24 @@ public:
 
 } // namespace stx
 
-#define STX_RESULT_HANDLED(X) {if(X){}}
+#define STX_RESULT_HANDLED(X) \
+	{                         \
+		if(X) {}              \
+	}
 
 #else // STX_DEBUG_TOOLS > 0
 
 namespace stx {
 
-template<typename T>
+template <typename T>
 using important = T;
 
-template<typename T>
+template <typename T>
 using asserted = T;
 
 } // namespace stx
 
-#define STX_RESULT_HANDLED(X) {X}
+#define STX_RESULT_HANDLED(X) \
+	{ X }
 
 #endif // STX_DEBUG_TOOLS > 0
