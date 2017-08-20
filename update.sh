d4177badc11bcd305e893b39cb86860402861997
Benno Straub <benno.straub@outlook.de>
2017-08-20 16:37:32 +0200

    Improved update.sh

--- update.sh
+++ update.sh
@@ -2,23 +2,25 @@
 
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-rm -r "${scriptdir}/include/x"* 2> /dev/null
-
-for header in "${scriptdir}/include/stx/"*
-do
-	if [ ! -d "${header}" ]
-	then
-		name=`basename ${header%.*}`
-		printf '#include "%s"' "stx/${name}.hpp" > "${scriptdir}/include/x${name}"
+function relink() {
+	if [ -z "$3" ]; then
+		if [ -d "${scriptdir:?}/$2" ]; then
+			rm -Rf   "${scriptdir:?}/$2"
+		fi
 	fi
-done
 
-mkdir "${scriptdir}/include/xwip/"
-for header in "${scriptdir}/include/stx/wip/"*.hpp
-do
-	if [ ! -d "${header}" ]
-	then
-		name=`basename ${header%.*}`
-		printf '#include "%s"' "../stx/wip/${name}.hpp" > "${scriptdir}/include/xwip/${name}"
-	fi
-done
+	mkdir -p "${scriptdir}/$2"
+
+	for header in "${scriptdir}/$1/"*.hpp
+	do
+		if [ ! -d "${header}" ]
+		then
+			name=`basename ${header%.*}`
+			outfile="${scriptdir}/$2/$3${name}"
+			printf '#include "%s"' "$(realpath --relative-to="$(dirname "${outfile}")" "${header}")" > "${outfile}"
+		fi
+	done
+}
+
+relink "include/stx/" "include/" "x"
+relink "include/stx/wip" "include/xwip"
