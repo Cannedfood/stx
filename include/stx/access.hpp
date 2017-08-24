7300e101fadbf6625820f20aae7db5ba395b9c18
Benno Straub <benno.straub@outlook.de>
2017-08-24 12:48:44 +0200

    Added access over virtual setter (xaccessv; xsetterv)

--- include/stx/access.hpp
+++ include/stx/access.hpp
@@ -16,3 +16,13 @@
 
 #define xaccess(ACCESSOR, MEMBER) \
 	xgetter(ACCESSOR, MEMBER) xsetter(ACCESSOR, MEMBER)
+
+// Virtual setter
+#define xsetterv(ACCESSOR, MEMBER)                \
+	virtual auto& ACCESSOR(decltype(MEMBER) const & m) { \
+		MEMBER = m;                              \
+		return *this;                            \
+	}
+
+#define xaccessv(ACCESSOR, MEMBER) \
+	xgetter(ACCESSOR, MEMBER) xsetterv(ACCESSOR, MEMBER)
