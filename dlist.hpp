f6aa44262b17f5b8b735dc5d9f743e1725b1e429
Benno Straub <benno.straub@outlook.de>
2018-06-08 20:27:39 +0200

    Implemented dlist fully

--- dlist.hpp
+++ dlist.hpp
@@ -2,10 +2,15 @@
 
 #pragma once
 
+#include <iterator>
+
 namespace stx {
 
 template<class T>
 class dlist;
+
+template<class T>
+class dlist_iterator;
 
 template<class Derived>
 class dlist_element;
@@ -14,8 +19,8 @@ class dlist_element;
 template<class Derived>
 class dlist_element {
 public:
-	using dlist_t = dlist<Derived>;
-	using dlist_element_t = dlist_element<Derived>;
+	using list_t = dlist<Derived>;
+	using element_t = dlist_element<Derived>;
 
 	dlist_element() :
 		m_next(nullptr),
@@ -27,7 +32,7 @@ public:
 
 	void push_tail(Derived** d) {
 		if(*d)
-			static_cast<dlist_element_t*>(*d)->insert_next((Derived*) this);
+			static_cast<element_t*>(*d)->insert_next((Derived*) this);
 		else
 			remove();
 		*d = static_cast<Derived*>(this);
@@ -62,25 +67,79 @@ public:
 	Derived* next() noexcept { return m_next; }
 	Derived* prev() noexcept { return m_prev; }
 
+	static Derived* next(Derived* d) noexcept {
+		return ((element_t*)d)->next();
+	}
+	static Derived* prev(Derived* d) noexcept {
+		return ((element_t*)d)->next();
+	}
+
 private:
 	Derived* m_next;
 	Derived* m_prev;
 };
 
 template<class T>
+class dlist_iterator : public std::iterator<std::bidirectional_iterator_tag, T> {
+	T* m_value;
+public:
+	using self_t    = dlist_iterator<T>;
+	using element_t = dlist_element<T>;
+
+	dlist_iterator(T* p = nullptr) noexcept : m_value(p) {}
+
+	T& operator*()  const { return *m_value; }
+	T* operator->() const noexcept { return m_value; }
+
+	self_t& operator++() noexcept {
+		m_value = element_t::next(m_value);
+		return *this;
+	}
+
+	self_t& operator--() noexcept {
+		m_value = element_t::prev(m_value);
+		return *this;
+	}
+
+	self_t operator++(int) const noexcept {
+		return { element_t::next(m_value) };
+	}
+
+	self_t operator--(int) const noexcept {
+		return { element_t::prev(m_value) };
+	}
+};
+
+template<class T>
 class dlist {
 public:
-	using dlist_element_t = dlist_element<T>;
-	using dlist_t         = dlist<T>;
+	using element_t = dlist_element<T>;
+	using list_t    = dlist<T>;
 
-	dlist() {
+	dlist() noexcept {
 		m_sentinel.m_next = static_cast<T*>(&m_sentinel);
 		m_sentinel.m_prev = static_cast<T*>(&m_sentinel);
 	}
 
 	bool empty() const noexcept { return m_sentinel.m_next == &m_sentinel; }
+
+	T* push_back(T* t)  noexcept { m_sentinel.insert_prev(t); }
+	T* push_front(T* t) noexcept { m_sentinel.insert_next(t); }
+
+	T*       front()       noexcept { return m_sentinel->next(); }
+	T*       back()        noexcept { return m_sentinel->prev(); }
+	T const* front() const noexcept { return m_sentinel->next(); }
+	T const* back()  const noexcept { return m_sentinel->prev(); }
+
+	using iterator         = dlist_iterator<T>;
+	using reverse_iterator = std::reverse_iterator<dlist_iterator<T>>;
+	iterator         begin() { return iterator{front()}; }
+	iterator         end()   { return iterator{back()}; }
+	reverse_iterator rbegin() { return reverse_iterator{front()}; }
+	reverse_iterator rend()   { return reverse_iterator{back()}; }
+
 private:
-	dlist_element_t m_sentinel;
+	element_t m_sentinel;
 };
 
 } // namespace stx
