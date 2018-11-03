195824afa91a2d21345222ce22eb7c5270a7ae6d
Benno Straub <benno.straub@outlook.de>
2018-11-03 16:58:37 +0100

    Fixed guid.hpp (Ordering and stuff)

--- guid.hpp
+++ guid.hpp
@@ -5,36 +5,35 @@
 namespace stx {
 
 template<class T>
-struct basic_guid {
-	T value;
+class basic_guid {
+	T m_value;
 
+public:
+	inline
 	basic_guid() noexcept :
-		value(generate())
+		m_value(generate())
 	{}
 
-	constexpr
+	constexpr inline
 	basic_guid(T value) noexcept :
-		value(value)
+		m_value(value)
 	{}
 
-	constexpr operator T() const noexcept { return value; }
-	constexpr bool operator==(basic_guid const& other) const noexcept { return value == other.value; }
-	constexpr bool operator!=(basic_guid const& other) const noexcept { return value != other.value; }
-	constexpr bool operator< (basic_guid const& other) const noexcept { return value <  other.value; }
-	constexpr bool operator> (basic_guid const& other) const noexcept { return value >  other.value; }
-	constexpr bool operator<=(basic_guid const& other) const noexcept { return value <= other.value; }
-	constexpr bool operator>=(basic_guid const& other) const noexcept { return value >= other.value; }
+	constexpr T value() const noexcept { return m_value; }
+	constexpr operator T() const noexcept { return value(); }
+
+	constexpr bool operator==(basic_guid const& other) const noexcept { return value() == other.value(); }
+	constexpr bool operator!=(basic_guid const& other) const noexcept { return value() != other.value(); }
+	constexpr bool operator< (basic_guid const& other) const noexcept { return value() <  other.value(); }
+	constexpr bool operator> (basic_guid const& other) const noexcept { return value() >  other.value(); }
+	constexpr bool operator<=(basic_guid const& other) const noexcept { return value() <= other.value(); }
+	constexpr bool operator>=(basic_guid const& other) const noexcept { return value() >= other.value(); }
+
 
 	static constexpr basic_guid null() noexcept { return {0}; }
 
 	static T generate() {
-		#if defined (_MSC_VER)  // Visual studio
-			#define thread_local __declspec( thread )
-		#elif defined (__GCC__) // GCC
-			#define thread_local __thread
-		#endif
-
-		static thread_local std::mt19937_64 generator{ std::random_device()() };
+		static std::mt19937_64 generator{ std::random_device()() };
 		return std::uniform_int_distribution<T>()(generator);
 	}
 };
