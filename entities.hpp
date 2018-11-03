74fe3985aefc83b064170c9ec7f6725df97275af
Benno Straub <benno.straub@outlook.de>
2018-11-03 16:01:12 +0100

    Improved entity system

--- entities.hpp
+++ entities.hpp
@@ -2,136 +2,375 @@
 
 // A entity component system
 
+// TODO:
+// - consolidate entities
+// - sort entities by components (and maybe by supplied function?)
+
 #pragma once
 
-#include <unordered_map>
+#include <memory>
 #include <vector>
-#include <cstdint>
+#include <bitset>
+#include <cassert>
 
 namespace stx {
 
-class entities;
+// =============================================================
+// == Settings =================================================
+// =============================================================
+
+constexpr inline size_t MaxNumComponents = 64;
+
+// =============================================================
+// == Component id =============================================
+// =============================================================
 
 namespace detail {
 
-class type_abstraction {
-	size_t m_alignment;
-public:
-	size_t alignment();
-
-	type_abstraction(size_t align) : m_alignment(align) {}
-
-	virtual void destroy(
-		uint8_t* data, size_t count, size_t stride) = 0;
-
-	virtual void move_assign(
-		size_t count,
-		uint8_t* src_data, size_t src_stride,
-		uint8_t* dst_data, size_t dst_stride) = 0;
-
-	virtual void move_construct(
-		size_t count,
-		uint8_t* src_data, size_t src_stride,
-		uint8_t* dst_data, size_t dst_stride) = 0;
-};
+size_t new_component_id() noexcept;
 
 template<class T>
-class type final : public type_abstraction {
+constexpr inline bool is_valid_component_v =
+	!std::is_pointer_v<T> &&
+	std::is_move_assignable_v<T> &&
+	std::is_move_constructible_v<T>;
+
+template<class T>
+constexpr inline bool is_nice_component_v =
+	is_valid_component_v<T> &&
+	std::is_trivially_destructible_v<T>;
+
+template<class T>
+struct component_id_holder {
+	static_assert(!std::is_pointer_v<T>, "Pointers are not valid components");
+	static_assert(std::is_move_assignable_v<T>, "Components have to be move assignable");
+	static_assert(std::is_move_constructible_v<T>, "Components have to be move constructable");
+	static size_t value;
+};
+template<class T> size_t component_id_holder<T>::value = new_component_id();
+
+} // namespace detail
+
+template<class T>
+inline           unsigned component_id = detail::component_id_holder<T>::value;
+constexpr inline unsigned invalid_component_id = ~unsigned(0);
+
+// =====================================================================
+// == Component allocation =============================================
+// =====================================================================
+
+class type_operations {
 public:
-	type() : type_abstraction(alignof(T)) {}
+	type_operations() {}
 
-	void destroy(uint8_t* data, size_t count, size_t stride) override {
-		for(size_t i = 0; i < count; i++) {
-			((T*)data + i * stride)->~T();
-		}
-	}
-
-	void move_assign(
-		size_t count,
-		uint8_t* src_data, size_t src_stride,
-		uint8_t* dst_data, size_t dst_stride) override
+	template<class T>
+	type_operations() :
+		type_operations()
 	{
-		for(size_t i = 0; i < count; i++) {
-			auto* src = ((T*) src_data + src_stride * i);
-			auto* dst = ((T*) dst_data + dst_stride * i);
-			*dst = std::move(*src);
+		static_assert(std::is_move_constructible_v<T>, "T has to be move constructable");
+		static_assert(std::is_move_assignable_v<T>,    "T has to be move assignable");
+		m_size = sizeof(T);
+		m_alignment = alignof(T);
+
+		m_pfnDestroy = [](void* a, void* b) { *static_cast<T*>(a)->~T(); };
+
+		m_pfnMoveAssign = [](void* a, void* b) { *static_cast<T*>(a) = std::move(*static_cast<T*>(b)); };
+		m_pfnMoveConstruct = [](void* at, void* b) { new(at) T(std::move(*static_cast<T*>(b))); };
+
+		if constexpr(std::is_copy_constructible_v<T>) {
+			m_pfnCopyConstruct = [](void* a, void* b) { *static_cast<T*>(a) = *static_cast<T*>(b); };
+		}
+
+		if constexpr(std::is_default_constructible_v<T>) {
+			m_pfnDefaultConstruct =  [](void* at) { new(at) T(); };
 		}
 	}
 
-	void move_construct(
-		size_t count,
-		uint8_t* src_data, size_t src_stride,
-		uint8_t* dst_data, size_t dst_stride) override
-	{
-		for(size_t i = 0; i < count; i++) {
-			auto* src = ((T*) src_data + src_stride * i);
-			auto* dst = ((T*) dst_data + dst_stride * i);
-			dst->T(std::move(*src));
-		}
-	}
+	size_t size() const noexcept { return m_size; }
+	size_t alignment() const noexcept { return m_alignment; }
+
+	void moveAssign      (void* from, void* to) { m_pfnMoveAssign(from, to); }
+	void moveConstruct   (void* from, void* to) { m_pfnMoveConstruct(from, to); }
+	void copyConstruct   (void* from, void* to) { m_pfnCopyConstruct(from, to); }
+	void copyAssign      (void* from, void* to) { m_pfnCopyAssign(from, to); }
+	void destroy         (void* at) { m_pfnDestroy(at); }
+	void defaultConstruct(void* at) { m_pfnDefaultConstruct(at); }
+
+private:
+	size_t m_size;
+	size_t m_alignment;
+
+	void (*m_pfnMoveAssign)       (void* from, void* to) = nullptr;
+	void (*m_pfnMoveConstruct)    (void* from, void* to) = nullptr;
+	void (*m_pfnCopyAssign)       (void* from, void* to) = nullptr;
+	void (*m_pfnCopyConstruct)    (void* from, void* to) = nullptr;
+	void (*m_pfnDestroy)          (void* at) = nullptr;
+	void (*m_pfnDefaultConstruct) (void* at) = nullptr;
 };
 
-class entity_class {
+// ================================================================
+// == Entity/EntityID =============================================
+// ================================================================
+
+/// A handle to an entity
+class entity {
+	uint64_t m_id;
+
 public:
-	size_t                             m_stride;
-	std::unordered_map<size_t, size_t> m_memory_layout;
-	std::vector<uint8_t>               m_memory;
+	constexpr entity() : m_id(~0ull) {}
+	constexpr entity(uint64_t id) : m_id(id) {}
+	constexpr entity(uint32_t index, uint32_t version) :
+		m_id(uint64_t(index) | uint64_t(version) << 32ull)
+	{}
 
-	std::set<entity_class*>  m_generalizations;
-	std::set<entity_class*>  m_specializations;
+	constexpr entity(entity const&) = default;
+	constexpr entity& operator=(entity const&) = default;
+
+	constexpr uint64_t id() const { return m_id; }
+	constexpr uint32_t index()   const { return m_id & 0xFFFFFFFF; }
+	constexpr uint32_t version() const { return m_id >> 32; }
+
+	constexpr operator bool() const noexcept { return *this != entity(); }
+
+	constexpr bool operator==(entity const& other) const noexcept { return id() == other.id(); }
+	constexpr bool operator!=(entity const& other) const noexcept { return id() != other.id(); }
+	constexpr bool operator<=(entity const& other) const noexcept { return id() <= other.id(); }
+	constexpr bool operator>=(entity const& other) const noexcept { return id() >= other.id(); }
+	constexpr bool operator< (entity const& other) const noexcept { return id() <  other.id(); }
+	constexpr bool operator> (entity const& other) const noexcept { return id() >  other.id(); }
 };
 
-template<class... Components>
-struct entity_class_binding {
-	std::array<size_t, sizeof...(Components)> m_component_layout;
+namespace detail {
 
-	entity_class_binding(entity_class& data) {
-		constexpr size_t hash_codes[] = { typeid(Components).hash_code()... };
-		for(size_t i = 0; i < sizeof...(Components); i++) {
-			m_component_layout[i] = data.m_memory_layout.at(hash_codes[i]);
-		}
-	}
-};
-
-template<class... Components>
-class filter_iterator {
+class id_manager {
 public:
-	// std::tuple<Components&..., uint64_t> operator*() {
-	//
-	// }
+	entity create() noexcept;
+	void   free(entity) noexcept;
+	bool   valid(entity) const noexcept;
 
-	filter_iterator& operator++() noexcept {
-		return *this; // TODO
-	}
-
-	bool operator!=(filter_iterator const& other) const noexcept {
-		return true; // TODO
-	}
+	entity entityAt(uint32_t index) const noexcept;
+private:
+	std::vector<uint32_t> m_versions;
+	std::vector<uint32_t> m_free;
 };
 
 } // namespace detail
 
-template<class... Components>
-class filter {
-	using entity_class_binding = detail::entity_class_binding<Components...>;
+// =====================================================================
+// == Component Allocation =============================================
+// =====================================================================
 
-	std::vector<entity_class_binding> m_bindings;
+class sparse_vector_interface {
 public:
-	using iterator = detail::filter_iterator<Components...>;
-
-	filter() {}
-	filter(entities& es) {}
-
-	iterator begin() {
-
-	}
-	iterator end() {
-
-	}
+	virtual void* getTypeErased    (size_t index) = 0;
+	virtual void  destroyTypeErased(size_t index) = 0;
+	virtual ~sparse_vector_interface() {}
 };
+
+template<class T, size_t ElementsPerBlock = 524288 / sizeof(T)>
+class sparse_vector final : public sparse_vector_interface {
+	constexpr size_t aligned_size() const noexcept {
+		return sizeof(T); // TODO: make sure multiples of the size are still aligned
+	}
+
+	void* getTypeErased(size_t index)     override { return get(index); }
+	void  destroyTypeErased(size_t index) override { return destroy(index); }
+public:
+	sparse_vector() {}
+	~sparse_vector() {}
+
+	T*   get  (size_t index) {
+		size_t block   = index / ElementsPerBlock; // Optimizes to right shift (hopefully)
+		size_t element = index % ElementsPerBlock; // Optimizes to bitwise-and
+		return reinterpret_cast<T*>(m_blocks[block].get() + element * aligned_size());
+	}
+
+	template<class... Args>
+	T*   create(size_t index, Args&&... args) {
+		_increment_usage(index);
+		return new(get(index)) T(std::forward<Args>(args)...);;
+	}
+	void destroy(size_t index) {
+		get(index)->~T();
+		_decrement_usage(index);
+	}
+	void moveToEmpty(size_t from, size_t to) {
+		T* pTo   = get(to);
+		T* pFrom = get(from);
+		new(pTo) T(std::move(*pFrom));
+		pFrom->~T();
+	}
+	void moveAndOverride(size_t from, size_t to) {
+		destroy(to);
+		moveToEmpty(from, to);
+	}
+
+	// Statistics
+	size_t total_slots() {
+		size_t result = 0;
+		for(auto& b : m_blocks) {
+			if(b) result += ElementsPerBlock;
+		}
+		return result;
+	}
+	size_t used_slots() {
+		size_t result = 0;
+		for(auto u : m_usages) result += u;
+		return result;
+	}
+	size_t unused_slots() { return total_slots() - used_slots(); }
+
+	size_t total_memory()  { return total_slots() * aligned_size(); }
+	size_t used_memory()   { return used_slots() * aligned_size(); }
+	size_t unused_memory() { return unused_slots() * aligned_size(); }
+
+private:
+	uint8_t* _increment_usage(size_t index) {
+		size_t block = index / ElementsPerBlock;
+
+		if(m_usages.size() <= block) {
+			m_usages.resize(block + 1);
+			m_blocks.resize(block + 1);
+		}
+
+		uint32_t usages = ++m_usages[block];
+		if(usages == 1) {
+			m_blocks[block].reset(new uint8_t[ElementsPerBlock * aligned_size()]);
+		}
+		return m_blocks[block].get();
+	}
+
+	void _decrement_usage(size_t index) {
+		size_t block = index / ElementsPerBlock;
+
+		if(--m_usages[block] == 0) {
+			m_blocks[block].reset();
+		}
+	}
+
+	std::vector<std::unique_ptr<uint8_t[]>> m_blocks;
+	std::vector<uint32_t>                   m_usages;
+};
+
+// ==============================================================
+// == Entity System =============================================
+// ==============================================================
+
+using component_mask = std::bitset<MaxNumComponents>;
 
 class entities {
 public:
+	entities();
+	~entities();
+
+	// Create/Destroy entities
+	entity create();
+	void   destroy(entity e);
+	bool   valid(entity e);
+
+	// Attach/Remove components
+	void* getUnchecked(entity e, size_t component_id);
+	void* get(entity e, size_t component_id);
+	void  remove(entity e, size_t component_id);
+
+	template<class T, class... Args>
+	T* attachNew(entity e, Args&&... args) {
+		assert(m_ids.valid(e));
+
+		using storage_t = sparse_vector<std::remove_reference_t<T>>;
+
+		size_t componentId = component_id<T>;
+		auto* storage = static_cast<storage_t*>(m_component_storage[componentId].get());
+		if(!storage) {
+			auto storagePtr = std::make_unique<storage_t>();
+			storage = storagePtr.get();
+			m_component_storage[componentId] = std::move(storagePtr);
+		}
+
+		auto& componentMask = m_component_masks[e.index()];
+		if(componentMask.test(componentId)) {
+			storage->destroy(e.index());
+		}
+		componentMask.set(componentId);
+		return storage->create(e.index(), std::forward<Args>(args)...);
+	}
+
+	template<class T>
+	T* attach(entity e, T&& t) { return attachNew<T>(e, std::forward<T>(t)); }
+
+	template<class T>
+	T* get(entity e) {
+		if(!m_component_masks[e.id()].test(component_id<T>)) return nullptr;
+		return getUnchecked<T>(e);
+	}
+	template<class T>
+	T* getUnchecked(entity e) {
+		auto* storage = static_cast<sparse_vector<T>*>(
+			m_component_storage[component_id<T>].get()
+		);
+		return storage->get(e.index());
+	}
+
+	// Filter entities by component
+	entity first(component_mask m);
+	entity next(entity e, component_mask m);
+private:
+	detail::id_manager          m_ids;
+	std::array<std::unique_ptr<sparse_vector_interface>, MaxNumComponents> m_component_storage;
+	std::vector<component_mask> m_component_masks;
+};
+
+// ====================================================================
+// == Component Filtering =============================================
+// ====================================================================
+
+template<class... Components>
+class filter {
+	component_mask m_mask;
+	entities&      m_entities;
+public:
+	filter(entities& entities) noexcept :
+		m_entities(entities)
+	{
+		for(unsigned id : std::array{ component_id<Components>... }) { // I <3 C++17
+			m_mask.set(id);
+		}
+	}
+
+	struct iterator {
+		mutable entities* m_entities;
+		entity            m_entity;
+		component_mask    m_mask;
+	public:
+		constexpr iterator() noexcept : m_entities(nullptr), m_entity(), m_mask() {}
+
+		constexpr iterator(entities* ecs, entity e, component_mask mask) noexcept
+			: m_entities(ecs), m_entity(e), m_mask(mask)
+		{}
+
+		iterator& operator++(void) noexcept {
+			m_entity = m_entities->next(m_entity, m_mask);
+			return *this;
+		}
+
+		std::tuple<Components&...> operator*() const noexcept {
+			return { *m_entities->getUnchecked<Components>(m_entity)... };
+		}
+
+		constexpr bool operator==(iterator const& other) const noexcept {
+			return m_entity == other.m_entity;
+		}
+		constexpr bool operator!=(iterator const& other) const noexcept {
+			return m_entity != other.m_entity;
+		}
+	};
+
+	iterator begin() const noexcept {
+		return iterator(&m_entities, m_entities.first(m_mask), m_mask);
+	}
+	constexpr iterator end() const noexcept {
+		return iterator();
+	}
 };
 
 } // namespace stx
---
+++ test/test_entities.cpp
@@ -0,0 +1,212 @@
+#include "catch.hpp"
+
+#include "../entities.hpp"
+
+#include <random>
+
+struct Counted {
+	struct Decrementer { void operator()(int* c) const noexcept { --*c; } };
+
+	std::unique_ptr<int, Decrementer> c;
+	Counted(int& c) : c(&(++c)) {}
+};
+
+std::vector<size_t> random_indices(size_t count, std::pair<size_t, size_t> minmax) {
+	std::vector<size_t> result;
+
+	size_t range = minmax.second - minmax.first;
+
+	result.reserve(range);
+	for(size_t i = minmax.first; i < minmax.second; i++) result.push_back(i);
+	std::shuffle(result.begin(), result.end(), std::mt19937(rand()));
+	result.resize(std::min(result.size(), count));
+	result.shrink_to_fit();
+	return result;
+}
+
+std::vector<size_t> random_indices(size_t count) {
+	return random_indices(count, {0, count});
+}
+
+TEST_CASE("entity id works", "[entities]") { ///////////////////////////////////
+	using namespace stx;
+
+	SECTION("Entity is properly constructed from id and version") {
+		CHECK(entity(0xFFFFFFFF, 0).index() == 0xFFFFFFFF);
+		CHECK(entity(0, 0xFFFFFFFF).version() == 0xFFFFFFFF);
+	}
+
+	SECTION("Entity default-constructs to invalid") {
+		CHECK(!entity());
+	}
+
+	SECTION("Entity behaves properly when copying them around") {
+		entity prototype = entity(0xAAAA5555, 0xFFFFFFFF);
+		std::vector<entity> entities(100, prototype);
+		std::shuffle(entities.begin(), entities.end(), std::mt19937(rand()));
+		for(auto e : entities)
+			REQUIRE(e == prototype);
+	}
+}
+
+TEST_CASE("id_manager works", "[entities]") { //////////////////////////////////
+	using namespace stx;
+
+	detail::id_manager manager;
+	int count = 5;
+
+	std::mt19937 rgen {(unsigned long)rand()};
+
+	std::vector<entity> entities;
+
+	for(size_t i = 0; i < 2; i++) {
+		entities.resize(count + i*count);
+
+		for(auto& e : entities) {
+			e = manager.create();
+			REQUIRE((bool)e);
+			REQUIRE(manager.valid(e));
+		}
+
+		std::sort(entities.begin(), entities.end());
+		REQUIRE(std::unique(entities.begin(), entities.end()) == entities.end());
+
+		std::shuffle(entities.begin(), entities.end(), rgen);
+
+		for(auto& e : entities) {
+			manager.free(e);
+			REQUIRE(!manager.valid(e));
+		}
+	}
+}
+
+TEST_CASE("Test sparse_memory", "[entities]") { ////////////////////////////////
+	stx::sparse_vector<Counted, 16> vec;
+
+	int count = 0;
+
+	SECTION("sparse_vector .get and .create return the same pointer") {
+		auto indices = random_indices(1000, {0, 128});
+
+		for(auto idx : indices) {
+			auto created = vec.create(idx, count);
+			auto gotten  = vec.get(idx);
+			REQUIRE(created == gotten);
+		}
+
+		for(auto idx : indices)
+			vec.destroy(idx);
+	}
+
+	SECTION("sparse_vector doesn't leak") {
+		auto indices = random_indices(1000, {0, 4096});
+
+		for(auto idx : indices) vec.create(idx, count);
+
+		REQUIRE(count == indices.size());
+		REQUIRE(vec.used_slots() == indices.size());
+		std::shuffle(indices.begin(), indices.end(), std::mt19937(rand()));
+
+		for(auto idx : indices) vec.destroy(idx);
+		REQUIRE(count == 0);
+		REQUIRE(vec.used_slots() == 0);
+		REQUIRE(vec.used_memory() == 0);
+	}
+}
+
+TEST_CASE("Test stx::entities", "[entities]") { ////////////////////////////////
+	SECTION("Attaching components works") {
+		int           count = 0;
+		stx::entities entities;
+
+		for(size_t i = 0; i < 1000; i++) {
+			stx::entity e = entities.create();
+			entities.attach(e, Counted{count});
+		}
+
+		REQUIRE(count == 1000);
+	}
+
+	SECTION("Getting components works") {
+		stx::entities entities;
+		for(size_t i = 0; i < 1000; i++) {
+			stx::entity e = entities.create();
+			entities.attach(e, int());
+			REQUIRE(entities.get<int>(e));
+			REQUIRE(entities.get<float>(e) == nullptr);
+		}
+	}
+
+	SECTION("doesn't leave memory leaks after destruction") {
+		int count = 0;
+		{
+			stx::entities entities;
+
+			for(size_t i = 0; i < 1000; i++) {
+				stx::entity e = entities.create();
+				entities.attach(e, Counted{count});
+			}
+
+			REQUIRE(count == 1000);
+		}
+		REQUIRE(count == 0);
+	}
+
+	SECTION("Manual iteration works") {
+		constexpr size_t Count = 100;
+
+		stx::entities entities;
+		for(size_t i = 0; i < Count; i++) {
+			entities.attach(entities.create(), int());
+		}
+		stx::component_mask mask;
+		mask.set(stx::component_id<int>);
+
+		size_t count = 0;
+		for(stx::entity e = entities.first(mask);
+		    e != stx::entity();
+		    e = entities.next(e, mask))
+		{
+			++count;
+		}
+		REQUIRE(count == Count);
+	}
+}
+
+TEST_CASE("Test stx::filter(stx::entities)", "[entities]") { ///////////////////
+	constexpr size_t Count = 1000;
+
+	using Foo = float;
+	using Bar = int;
+
+	stx::entities entities;
+	for(size_t i = 0; i < Count; i++) {
+		stx::entity e = entities.create();
+		entities.attach(e, Foo());
+		entities.attach(e, Bar());
+	}
+
+	SECTION("Filtering by Foo works") {
+		size_t count = 0;
+		for(auto [foo] : stx::filter<Foo>(entities)) {
+			++count;
+		}
+		REQUIRE(count == Count);
+	}
+
+	SECTION("Filtering by Bar works") {
+		size_t count = 0;
+		for(auto [bar] : stx::filter<Foo>(entities)) {
+			++count;
+		}
+		REQUIRE(count == Count);
+	}
+
+	SECTION("Filtering by Foo and Bar works") {
+		size_t count = 0;
+		for(auto [foo, bar] : stx::filter<Foo, Bar>(entities)) {
+			++count;
+		}
+		REQUIRE(count == Count);
+	}
+}
