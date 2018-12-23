fdee8bcf278b53721f651f62f718be77c7490618
Benno Straub <benno.straub@outlook.de>
2018-12-23 13:26:38 +0100

    Added simple injector

--- include/stx/injector.hpp
+++ include/stx/injector.hpp
@@ -1,14 +1,66 @@
 #pragma once
 
 #include <unordered_map>
+#include <array>
+#include <functional>
 
 #include "shared_ptr.hpp"
 
 namespace stx {
 
 class injector {
-	injector* m_fallback;
 public:
+	using entry   = std::shared_ptr<void>;
+	using factory = std::function<entry(injector&)>;
+	using factory_listener = std::function<void(entry const&, injector&)>;
+
+	entry request   (std::type_info const& info, size_t quirk);
+	void  add       (entry   e, std::type_info const& info, size_t quirk);
+	void  addFactory(factory f, std::type_info const& info, size_t quirk);
+	void  addFactoryListener(factory_listener f, std::type_info const& info, size_t quirk);
+
+	template<class T> std::shared_ptr<T> request(size_t quirk = 0);
+	template<class T> void add(std::shared_ptr<T>, size_t quirk = 0);
+	template<class T> void add(size_t quirk = 0);
+	template<class T> void addFactory(factory f, size_t quirk = 0);
+	template<class T> void addFactory(size_t quirk = 0);
+
+	template<class T>
+	operator std::shared_ptr<T>() {
+		return std::static_pointer_cast<T>(request(typeid(T), 0));
+	}
+public:
+	std::unordered_map<size_t, entry>   m_entries;
+	std::unordered_map<size_t, factory> m_factories;
 };
+
+} // namespace stx
+
+// =============================================================
+// == Inline implementation ====================================
+// =============================================================
+
+namespace stx {
+
+template<class T>
+std::shared_ptr<T> injector::request(size_t quirk) {
+	return std::static_pointer_cast<T>(request(typeid(T), quirk));
+}
+template<class T>
+void injector::add(size_t quirk) {
+	add(std::make_shared<T>(), typeid(T), quirk);
+}
+template<class T>
+void injector::add(std::shared_ptr<T> e, size_t quirk) {
+	add(e, typeid(T), quirk);
+}
+template<class T>
+void injector::addFactory(size_t quirk) {
+	addFactory([](auto& injector) { return std::make_shared<T>(); }, typeid(T), quirk);
+}
+template<class T>
+void injector::addFactory(factory f, size_t quirk) {
+	addFactory(f, typeid(T), quirk);
+}
 
 } // namespace stx
---
+++ include/stx/src/injector.cpp
@@ -0,0 +1,46 @@
+#include "../injector.hpp"
+
+#ifndef NDEBUG
+#include "../type.hpp"
+#endif
+
+namespace stx {
+
+static size_t get_id(std::type_info const& info, size_t quirk) {
+	return (info.hash_code() + quirk) ^ quirk;
+}
+
+injector::entry injector::request   (std::type_info const& info, size_t quirk) {
+	auto id = get_id(info, quirk);
+
+	auto iter = m_entries.find(id);
+	if(iter != m_entries.end()) return iter->second;
+
+	auto factory_iter = m_factories.find(id);
+	if(factory_iter == m_factories.end())
+		throw std::runtime_error("Couldn't find " + std::string(info.name()));
+
+	#ifndef NDEBUG
+		static int depth = 0;
+		printf(
+			"[stx:injector](info) %.*s%sCreating %s\n",
+			std::clamp(depth * 2 - 3, 0, 32), "                                  ",
+			depth > 0 ? u8" ⮡ " : "",
+			stx::demangle(info.name()).c_str());
+		depth++;
+	#endif
+	auto result = m_entries.emplace_hint(iter, id, factory_iter->second(*this))->second;
+	#ifndef NDEBUG
+		depth--;
+	#endif
+
+	return result;
+}
+void  injector::add       (entry   e, std::type_info const& info, size_t quirk) {
+	m_entries.try_emplace(get_id(info, quirk), e);
+}
+void  injector::addFactory(factory f, std::type_info const& info, size_t quirk) {
+	m_factories.try_emplace(get_id(info, quirk), f);
+}
+
+} // namespace stx
